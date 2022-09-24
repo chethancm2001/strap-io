9af3b349cbefb3c160e8a659fc9d9168e28f06b2
chethancm2001 <chethancm2001kadur@gmail.com>
2022-09-24 17:41:35 +0530

    added production environament

--- config/config.js
+++ config/config.js
@@ -4,14 +4,17 @@ const webpush = require('web-push')
 
 module.exports = function config(){
   let mongoDbUrl = ''
-if(NODE_ENV == 'production'){
+  
+if(process.env.NODE_ENV.trim() == 'production'){
   console.log("runing the production environament")
   mongoDbUrl = process.env.PRODUCTION_DB
 }
-else if(NODE_ENV == 'development'){
+
+if(process.env.NODE_ENV.trim() == 'development'){
   mongoDbUrl = process.env.DEVELOPMENT_DB
 console.log("Runing the development environament")
 }
+
 
 mongoose.connect(mongoDbUrl)
 .then(()=>{console.log("connted to db");}).catch(()=>{console.log("failed to connect to db")})
