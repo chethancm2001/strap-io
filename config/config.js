5453edbac6a699574492597569ad9b3d4b21a1e4
chethancm2001 <chethancm2001kadur@gmail.com>
2022-09-24 17:04:20 +0530

    added configaration for development and prodection environament

--- config/config.js
+++ config/config.js
@@ -1,9 +1,19 @@
 const mongoose = require('mongoose')
 require('dotenv').config()
 const webpush = require('web-push')
-module.exports = function config(){
 
-mongoose.connect(process.env.MONGOURL)
+module.exports = function config(){
+  let mongoDbUrl = ''
+if(NODE_ENV == 'production'){
+  console.log("runing the production environament")
+  mongoDbUrl = process.env.PRODUCTION_DB
+}
+else if(NODE_ENV == 'development'){
+  mongoDbUrl = process.env.DEVELOPMENT_DB
+console.log("Runing the development environament")
+}
+
+mongoose.connect(mongoDbUrl)
 .then(()=>{console.log("connted to db");}).catch(()=>{console.log("failed to connect to db")})
 
 
